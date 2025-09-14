efc57cf7e373bdd445ac1cd8db159fe2210eac08
Nikolay Topkaridi <enemycnt@gmail.com>
2025-09-14 19:44:50 +0400

    updated configs

--- .eslintrc.cjs
+++
@@ -1,7 +0,0 @@
-/* eslint-env node */
-module.exports = {
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
-  root: true,
-};
