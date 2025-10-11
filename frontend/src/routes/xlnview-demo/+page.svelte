93fe75ab0c1600fb88ed49f4fb28b6f307f03055
Egor Homakov <homakov@gmail.com>
2025-10-11 15:38:00 +0300

    Add XLNView demo route for testing

---
+++ frontend/src/routes/xlnview-demo/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+  /**
+   * XLNView Demo Route
+   * Test the new panel workspace
+   */
+  import XLNView from '../../../../view/XLNView.svelte';
+</script>
+
+<svelte:head>
+  <title>XLNView Demo - Panel Workspace</title>
+</svelte:head>
+
+<XLNView layout="default" networkMode="simnet" />
+
+<style>
+  :global(body) {
+    margin: 0;
+    overflow: hidden;
+  }
+</style>
