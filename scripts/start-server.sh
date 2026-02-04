d5afc787d287cb19b7480f0893981d2906c81e87
Egor Homakov <homakov@gmail.com>
2026-02-04 03:03:39 +0300

    Set PUBLIC_RPC for prod jurisdictions

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -6,6 +6,7 @@ cd /root/xln
 
 export USE_ANVIL=true
 export ANVIL_RPC=http://localhost:8545
+export PUBLIC_RPC=${PUBLIC_RPC:-https://xln.finance/rpc}
 export PATH="/root/.bun/bin:$PATH"
 
 exec /root/.bun/bin/bun runtime/server.ts --port 8080
